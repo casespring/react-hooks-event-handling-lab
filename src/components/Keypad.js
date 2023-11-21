e1d1160a714ccd4aae09492835e05caddd4baefd
Marco Upia <markremembers@gmail.com>
2023-11-21 09:27:02 -0500

    solve

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function changeHandler(e) {
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={changeHandler}></input>
+        </div>
     )
 }
 
